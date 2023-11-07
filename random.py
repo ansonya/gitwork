35838b3208ca8d51f91a76b7e7da7de22417c0e7
omkanekar28 <omkanekar28@LAPTOP-C401J7LC.localdomain>
2023-11-07 12:14:17 +0530

    Added turtle function

--- random.py
+++ random.py
@@ -1,4 +1,4 @@
-print("Hello World!")
+import turtle
 
 def old():
     print("Hello World!")
@@ -7,6 +7,14 @@ def old():
 
 def new():
     return "2nd commit by dhan "
+
+def star():
+    ws = turtle.Screen()
+    geekyTurtle = turtle.Turtle()
+ 
+    for i in range(5):
+        geekyTurtle.forward(100)
+        geekyTurtle.right(144)
 
 new()
     
