1455409843b49a4b8454a634cd67e25f6ffd5215
omkanekar28 <omkanekar28@LAPTOP-C401J7LC.localdomain>
2023-11-07 10:47:06 +0530

    random.py changed

--- random.py
+++ random.py
@@ -1,7 +1,13 @@
 print("Hello World!")
 
+def old():
+    print("Hello World!")
+    print("Hello World!")
+    print("Hello World!")
+
 def new():
     return "2nd commit by dhan "
 
 new()
     
+
